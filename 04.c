20ce6de9a1141461c1ffeb9fe3c35abb919ba6a7
Rupok saha <142249877+rupoksaha2023@users.noreply.github.com>
2023-08-22 11:00:11 +0600

    Update 04.c

--- 04.c
+++ 04.c
@@ -1,33 +1,32 @@
-#include<stdio.h>
-int main () {
-double student_number ;
+#include <stdio.h>
+int main (){
+double student_number;
 printf ("Input Student Number :");
 scanf ("%lf", &student_number);
-int intStudentNumber = (int)student_number;
-switch(intStudentNumber)
+int intstudentnumber = (int)student_number;
+switch (intstudentnumber)
 {
-case 80 ... 100:
-printf("Grade A+\n");
-break;
-case 70 ... 79:
-printf("Grade A\n");
+case 80 ... 100 :
+    printf ("Grade A+\n");
+    break;
+    case 70 ... 79 :
+    printf ("Grade A\n");
     break;
     case 60 ... 69:
-    printf("Grade B\n");
+    printf ("Grade B\n");
     break;
     case 50 ... 59:
-    printf("Grade C\n");
+    printf ("Grade C\n");
     break;
     case 40 ... 49:
-    printf("Grade D\n");
+    printf ("Grade D\n");
     break;
     case 0 ... 39:
-    printf("Grade F\n");
+    printf ("Grade F\n");
     break;
-    default:
-        printf("Invalid input\n");
+    default :
+        printf ("Invalid input \n");
         break;
 }
 return 0 ;
-
 }
